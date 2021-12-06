c51aaab04ce4bc144d374ed7121e681ed4fdca7c
shinya852 <shinya.june@gmail.com>
2021-12-06 10:51:02 +0000

    AI 言語

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -6,8 +6,8 @@ class ReptilesController < ApplicationController
 
   def create
     @reptile = Reptile.new(reptile_params)
-    @reptile.user_id = current_user.id
     @reptile.score = Language.get_data(reptile_params[:caption])
+    @reptile.user_id = current_user.id
     @reptile.save
     redirect_to reptiles_path
   end
@@ -30,17 +30,14 @@ class ReptilesController < ApplicationController
   end
 
   def edit
-    #@reptile = Reptile.find(params[:id])
     @reptile = Reptile.find(params[:id])
   end
 
   def update
     @reptile = Reptile.find(params[:id])
-    if @reptile = Reptile.update(reptile_params)
-      redirect_to reptiles_path
-    else
-      render 'edit'
-    end
+    @reptile.score = Language.get_data(reptile_params[:caption])
+    @reptile.update(reptile_params)
+    redirect_to reptiles_path
   end
 
 
